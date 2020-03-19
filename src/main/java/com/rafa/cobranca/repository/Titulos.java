b41fb8e071bf5e1cbecf314b6068c58f501850e6
Rafa556 <fernandesrafael149@gmail.com>
2020-03-19 15:24:12 -0300

    Segundodia

---
+++ src/main/java/com/rafa/cobranca/repository/Titulos.java
@@ -0,0 +1,9 @@
+package com.rafa.cobranca.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rafa.cobranca.model.Titulo;
+
+public interface Titulos extends JpaRepository<Titulo, Long> {
+
+}
